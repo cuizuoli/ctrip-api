5594f039a06b233ac30c4283b2fc3b07e17546ec
cuizuoli <cuizuoli@cuizuoli-PC>
2014-01-25 23:02:11 +0800

    init

---
+++ src/main/java/com/ctrip/api/Hotel.java
@@ -0,0 +1,18 @@
+/*
+ * @(#)Hotel.java $version 2014年1月25日
+ *
+ * Copyright 2014 Vip Journey. All rights Reserved.
+ * Vip Journey PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.ctrip.api;
+
+/**
+ * vip-journey ctrip-api
+ * com.ctrip.api.Hotel.java
+ * @author cuizuoli
+ * @date 2014年1月25日
+ */
+public class Hotel {
+
+}
---
+++ src/main/java/com/ctrip/api/Payment.java
@@ -0,0 +1,18 @@
+/*
+ * @(#)Payment.java $version 2014年1月25日
+ *
+ * Copyright 2014 Vip Journey. All rights Reserved.
+ * Vip Journey PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.ctrip.api;
+
+/**
+ * vip-journey ctrip-api
+ * com.ctrip.api.Payment.java
+ * @author cuizuoli
+ * @date 2014年1月25日
+ */
+public class Payment {
+
+}
